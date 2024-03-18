4c3afa6587b660ec7e546b960fee52340a80f8fd
alzamora2000 <162034762+alzamora2000@users.noreply.github.com>
2024-03-18 03:23:35 +0000

    solucion10

---
+++ solucion10.py
@@ -0,0 +1,8 @@
+fecha = input("Introduce la fecha de tu nacimiento en formato mm/dd/aaaa: ")
+#fecha = input("Introduce la fecha de tu nacimiento en formato dd/mm/aaaa: ")
+print('año', fecha[6:10])
+print('Mes', fecha[3:5])
+print('dia', fecha[:2])
+
+fecha2 = print("Formato AAAA-MM-DD:"+fecha[6:10]+"/"+fecha[:2]+"/"+fecha[3:5])
+
